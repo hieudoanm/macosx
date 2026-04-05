4cf57ef28ef60dca949a4192001e8b6542af737a
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-04-05 23:32:52 +0700

    move wifi

--- packages/app/src-tauri/src/main.rs
+++ packages/app/src-tauri/src/main.rs
@@ -1,10 +1,10 @@
-mod utils;
+mod wifi;
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            utils::wifi::list_wifi_networks,
-            utils::wifi::connect_wifi
+            wifi::list_wifi_networks,
+            wifi::connect_wifi
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
